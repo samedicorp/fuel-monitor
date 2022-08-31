d6d5977cc59f106cfe09aed93cac466ef6ae1874
Sam Deane <sam@elegantchaos.com>
2022-08-31 19:01:42 +0100

    first commit

---
+++ configure.lua
@@ -0,0 +1,20 @@
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+--  Created by Samedi on 27/08/2022.
+--  All code (c) 2022, The Samedi Corporation.
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+local useLocal = true --export: Use require() to load local scripts if present. Useful during development.
+local logging = true --export: Enable controller debug output.
+
+modulaSettings = { 
+    name = "Container Monitor",
+    version = "1.0",
+    logging = logging, 
+    useLocal = useLocal,
+    modules = {
+        ["samedicorp.container-monitor.module"] = { },
+        ["samedicorp.modula.modules.screen"] = { }
+    }
+}
+
+
